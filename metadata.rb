48f103ced1470ae00ae1bb682fe60b26f8fb55b9
martin-stevens <martin.stevens@salesforce.com>
2017-05-13 20:38:11 +0100

    Updated metadata

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,14 @@
 name 'budge-base'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
+maintainer 'Martin Stevens'
+maintainer_email 'budgester@budgester.com'
+license 'Apache-2.0'
 description 'Installs/Configures budge-base'
 long_description 'Installs/Configures budge-base'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
+supports 'centos'
+supports 'windows'
+
 issues_url 'https://github.com/budgester/budge-base/issues'
- source_url 'https://github.com/budgester/budge-base'
+source_url 'https://github.com/budgester/budge-base'
